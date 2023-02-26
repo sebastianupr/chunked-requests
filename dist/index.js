027d05986eef1ff49bae01cfca85e5ae18c4be4a
Sebastián Urbano <42834763+sebastianupr@users.noreply.github.com>
2023-02-26 03:40:30 -0400

    feature: README 0.1.0 (#7)
    * patch: example adjustments
    
    * feature: export params type

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fetch_chunked_requests_1 = require("./fetch-chunked-requests");
-exports.default = fetch_chunked_requests_1.default;
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import fetchChunkedRequests from './fetch-chunked-requests'
+import type { Params } from './types'
 
 export default fetchChunkedRequests
+
+export type { Params }
